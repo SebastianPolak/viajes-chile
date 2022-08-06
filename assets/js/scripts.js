e564e3ceecec37d2fa3150502c727b41b496681d
Seba Polak <sebapolakdisca@MBP-de-Seba.fibertel.com.ar>
2022-08-06 02:22:42 -0300

    modificando navbar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,6 +16,12 @@ $(document).ready(function(){
         alert("El mensaje fue enviado correctamente...");
       });
 
+      $(document).scroll(function(){
+        const y = $('html').scrollTop();
+
+        y > 1100 ? $('nav').addClass('nav-black') : $('nav').removeClass("nav-black");
+      })
+
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 })
